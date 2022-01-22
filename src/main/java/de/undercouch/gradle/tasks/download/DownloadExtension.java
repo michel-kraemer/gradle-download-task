f9280a0292feb87e34b620221caa6643eaccc8f7
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-01-22 15:12:05 +0100

    Make download extension compatible with Kotlin and Gradle 8
    Fixes #150

--- src/main/java/de/undercouch/gradle/tasks/download/DownloadExtension.java
+++ src/main/java/de/undercouch/gradle/tasks/download/DownloadExtension.java
@@ -1,32 +1,13 @@
-// Copyright 2013 Michel Kraemer
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package de.undercouch.gradle.tasks.download;
 
-import groovy.lang.Closure;
-
-import java.io.IOException;
-
+import org.gradle.api.Action;
 import org.gradle.api.Project;
-import org.gradle.util.Configurable;
-import org.gradle.util.ConfigureUtil;
 
 /**
- * An extension that configures and executes a {@link DownloadAction}
+ * An extension that executes a {@link DownloadAction}
  * @author Michel Kraemer
  */
-public class DownloadExtension implements Configurable<DownloadExtension> {
+public class DownloadExtension {
     private final Project project;
     
     /**
@@ -36,10 +17,14 @@ public class DownloadExtension implements Configurable<DownloadExtension> {
     public DownloadExtension(Project project) {
         this.project = project;
     }
-    
-    @Override
-    public DownloadExtension configure(Closure cl) {
-        DownloadAction da = ConfigureUtil.configure(cl, new DownloadAction(project));
+
+    /**
+     * Download a file now
+     * @param action action that configures a given {@code DownloadSpec}
+     */
+    public void run(Action<DownloadSpec> action) {
+        DownloadAction da = new DownloadAction(project);
+        action.execute(da);
         try {
             da.execute().get();
         } catch (Exception e) {
@@ -49,6 +34,5 @@ public class DownloadExtension implements Configurable<DownloadExtension> {
             }
             throw new IllegalStateException(message, e);
         }
-        return this;
     }
 }
