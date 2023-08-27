0af2136367e13b813cf78ab6f77cbb4471904619
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-27 14:59:06 +0200

    Bump up version number to 5.5.0

--- examples/kotlin/lazy-src-and-dest/build.gradle.kts
+++ examples/kotlin/lazy-src-and-dest/build.gradle.kts
@@ -2,7 +2,7 @@
  * Include the gradle-download-task plugin
  */
 plugins {
-    id("de.undercouch.download") version "5.4.0"
+    id("de.undercouch.download") version "5.5.0"
 }
 
 import java.nio.file.Files
